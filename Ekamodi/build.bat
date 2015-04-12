9beac60407ff41e778ce5d5d10664dba491508b0
PollexD <pollexd@gmail.com>
2015-04-12 17:12:07 +0300

    bugi msbuildssä

--- Ekamodi/build.bat
+++ Ekamodi/build.bat
@@ -2,7 +2,8 @@ set MODBUDDY=C:\Program Files (x86)\Steam\steamapps\common\Sid Meier's Civilizat
 set MODS_DIR=C:\Users\peluri\Documents\My Games\Sid Meier's Civilization 5\MODS
 set MOD_NAME=Monnin Sivilisaatio - Johtaja Eetu
 
-"%MODBUDDY%" Ekamodi.civ5sln /project Liideri /rebuild "Default|x86"
+rem "%MODBUDDY%" Ekamodi.civ5sln /project Liideri /clean "Default|x86"
+rem "%MODBUDDY%" Ekamodi.civ5sln /project Liideri /build "Default|x86"
 
 rmdir /Q /S "%MODS_DIR%\Mods"
 rmdir /Q /S "%MODS_DIR%\%MOD_NAME%"
