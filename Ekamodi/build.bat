ebdaac225830ee362fcb8264fef0536348874972
PollexD <pollexd@gmail.com>
2015-04-14 13:02:17 +0300

    Civin Iconi Toiminnassa

--- Ekamodi/build.bat
+++ Ekamodi/build.bat
@@ -1,5 +1,5 @@
 set MODBUDDY=C:\Program Files (x86)\Steam\steamapps\common\Sid Meier's Civilization V SDK\ModBuddy\ModBuddy.exe
-set MODS_DIR=C:\Users\peluri\Documents\My Games\Sid Meier's Civilization 5\MODS
+set MODS_DIR=C:\Users\lapset_2.kotikone\Documents\My Games\Sid Meier's Civilization 5\MODS
 set MOD_NAME=Monnin Sivilisaatio - Johtaja Eetu
 
 rem "%MODBUDDY%" Ekamodi.civ5sln /project Liideri /clean "Default|x86"
